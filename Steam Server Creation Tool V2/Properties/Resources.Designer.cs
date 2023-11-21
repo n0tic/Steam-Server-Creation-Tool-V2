4e88f1141b055820c2cfb15cc93f977aeb9cafae
N0tiC <n0tic@live.se>
2023-11-21 22:16:01 +0100

    Updates to screenshots

--- Steam Server Creation Tool V2/Properties/Resources.Designer.cs
+++ Steam Server Creation Tool V2/Properties/Resources.Designer.cs
@@ -223,6 +223,16 @@ namespace Steam_Server_Creation_Tool_V2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ConsoleWindow {
+            get {
+                object obj = ResourceManager.GetObject("ConsoleWindow", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Donate {
             get {
                 object obj = ResourceManager.GetObject("Donate", resourceCulture);
@@ -286,6 +296,16 @@ namespace Steam_Server_Creation_Tool_V2.Properties {
         internal static System.Drawing.Bitmap NewServer {
             get {
                 object obj = ResourceManager.GetObject("NewServer", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap PortScan {
+            get {
+                object obj = ResourceManager.GetObject("PortScan", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
