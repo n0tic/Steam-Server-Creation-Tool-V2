2f337d5536fd9ad3d55f011e49fcfe6a218b432f
N0tiC <n0tic@live.se>
2026-01-09 23:27:25 +0100

    Version update

--- Steam Server Creation Tool V2/Core/Core.cs
+++ Steam Server Creation Tool V2/Core/Core.cs
@@ -37,7 +37,7 @@ namespace Steam_Server_Creation_Tool_V2
         public static BuildTypes buildType = BuildTypes.Alpha;
         public static int majorVersion = 0;
         public static int minorVersion = 0;
-        public static int buildVersion = 12;
+        public static int buildVersion = 13;
 
         private static bool checkingUpdate = false;
         public static bool updateAvailable = false;
